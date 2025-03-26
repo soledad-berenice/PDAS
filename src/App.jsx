2f630db4f7d528979fb92934918d8b46f26f2caa
soledad-berenice <dominiicsoledad@gmail.com>
2025-03-25 19:00:30 -0700

    Agregue el ProfileCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
+import ProfileCard from "./ProfileCard"
+
 function App() {
 
   return (
     <>
       <h1>Componente App</h1>
-    </>
+        <section className="hero is-dark">
+          <div className="hero-body">
+            <p className="title">Personal Digital Asistents</p>
+          </div>
+        </section>
+        <div className="container">
+          <section className="section">
+            <div className="columns">
+              <div className="column is-4">
+                <ProfileCard />
+                
+              </div>
+              <div className="column is-4">
+                <ProfileCard />
+                
+              </div>
+              <div className="column is-4">
+                <ProfileCard 
+                title="Alexa"
+                handle="@alexa24"
+                image=""https://upload.wikimedia.org/wikipedia/commons/c/cc/Amazon_Alexa_App_Logo.png
+                />
+                
+              </div>
+            </div>
+          </section>
+        </div>
+      </>
   )
 }
 
---
+++ src/ProfileCard.jsx
@@ -0,0 +1,10 @@
+function ProfileCard({ title, handle, image }) {
+    console.log(title)
+    console.log(handle)
+    console.log(image)
+  return (
+    <div>ProfileCard</div>
+  )
+}
+
+export default ProfileCard
