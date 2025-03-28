bcf8bd0595db415597cc59481e6bae694073d64c
soledad-berenice <dominiicsoledad@gmail.com>
2025-03-27 19:40:32 -0700

    Agregado App

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,9 +1,20 @@
-function ProfileCard({ title, handle, image }) {
-    console.log(title)
-    console.log(handle)
-    console.log(image)
-  return (
-    <div>ProfileCard 2</div>
+function ProfileCard({ title, handle, image}) {
+
+  return(
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="" vidth={200}height={200} />
+        </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media-content">
+        <p className="title is-4"> Title is {title}</p>
+        <p className="subtitle is-6"> Handle is {handle} </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
